7eb92851ed39dab1019f0b4875fb62fb84c5cf42
laurahohlsiepe <l.hohlsiepe@gmail.com>
2022-03-08 16:54:13 +0100

    commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let todoList = document.querySelector("#newUl");
+let todoInput = document.querySelector("#input");
+let todobtn = document.querySelector("#to-do-btn");
+
+let deletebtn = document.querySelector("#delete-btn");
+
+todobtn.addEventListener("click", addNewItem);
+
+deletebtn.addEventListener("click", removeItem);
+
+function addNewItem() {
+  const newTextNode = todoInput.value;
+  const li = document.createElement("li");
+  const todoTextNode = document.createTextNode(newTextNode);
+  li.appendChild(todoTextNode);
+  todoList.appendChild(li);
+  todoInput.value = "";
+}
+
+function removeItem() {
+  todoList.innerHTML = "";
+}
